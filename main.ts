2b5517cdd7e6cda9550a047b8291fba7778976ad
cubscout-inventor <110574897+cubscout-inventor@users.noreply.github.com>
2022-08-07 16:55:16 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let myNumber = 0
+basic.showString("Compass For a Blind Person")
 basic.forever(function () {
     myNumber = Math.map(input.compassHeading(), 0, 359, 200, 3000)
     music.playTone(myNumber, music.beat(BeatFraction.Quarter))
